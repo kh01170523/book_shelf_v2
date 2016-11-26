37ecd8bb1e37fa0379df0f32be053d855d90a7a7
watanabe_shuichi <kh01170523@gmail.com>
2016-11-26 06:28:29 +0000

    リファクタリング

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,11 @@
 class BooksController < ApplicationController
+  before_action :set_book,only:[:show,:edit,:update,:destroy]
   def index
     @books = Book.all
   end
   
   def show
-    @book = Book.find(params[:id])
+    #@book = Book.find(params[:id])
   end
   
   def new
@@ -20,21 +21,35 @@ class BooksController < ApplicationController
   end
   
   def edit
-    @book = Book.find(params[:id])
+    #@book = Book.find(params[:id])
   end
   
   def update
-    @book = Book.find(params[:id])
+    #@book = Book.find(params[:id])
     @book.update(book_params)
     
     # リダイレクト
+    redirect_to book_path
+  end
+  
+  def destroy
+    #@book = Book.fine(params[:id])
+    @book.destroy
+    
     redirect_to books_path
+    
+  end
+  
+  private
+  def set_book
+    @book = Book.find(params[:id])
   end
   
    private
   def book_params
     params.require(:book).permit(:name,:price,:publish,:publish_date)
   end
+  
   
   
   
