1e90ab2f69746161535fa2bb964f8404f93b3b95
watanabe_shuichi <kh01170523@gmail.com>
2016-11-26 04:23:15 +0000

    新規追加機能作成まで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,28 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
+  
+  def show
+    @book = Book.find(params[:id])
+  end
+  
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    
+    # リダイレクト
+    redirect_to books_path
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:name,:price,:publish,:publish_date)
+  end
+  
+  
 end
